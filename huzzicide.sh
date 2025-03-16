2c5a9194bd8f58066ed2961cfd85b10ed804b246
aLEGEND21 <arnavmurthi@gmail.com>
2025-03-16 16:38:30 -0400

    build: Rename folder

--- huzzicide.sh
+++ huzzicide.sh
@@ -2,7 +2,7 @@
 echo y | docker image prune
 cp Huzzicide/.env h.env
 rm -rf Huzzicide
-git clone https://github.com/aLEGEND21/Scrapyard-Huzzicide.git
+git clone https://github.com/aLEGEND21/Scrapyard-Huzzicide.git Huzzicide
 cp h.env Huzzicide/.env
 rm -rf h.env
 cd Huzzicide
