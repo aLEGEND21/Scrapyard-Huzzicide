f696b201a12394366a7646b10e80620bb9ed3be8
aLEGEND21 <arnavmurthi@gmail.com>
2025-03-16 17:05:59 -0400

    chore: Fix backend url

--- frontend/lib/env.ts
+++ frontend/lib/env.ts
@@ -1,4 +1,4 @@
-let BACKEND_URL = "https://huzzicide.greenapp.live/api"
+let BACKEND_URL = "https://huzzicide.greenapp.tech/api"
 if (process.env.NODE_ENV === "development") {
     BACKEND_URL = "http://localhost:3007";
 }
